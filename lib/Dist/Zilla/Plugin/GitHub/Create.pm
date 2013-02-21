b8e3360330260c70950af2e9990a29cf04f41a8a
Alessandro Ghedini <alessandro@ghedini.me>
2013-02-21 10:34:46 +0100

    Create.pm: add link to Dist::Zilla::Plugin::Git::PushInitial

--- lib/Dist/Zilla/Plugin/GitHub/Create.pm
+++ lib/Dist/Zilla/Plugin/GitHub/Create.pm
@@ -217,7 +217,8 @@ This may allow one to use the L<Dist::Zilla::Plugin::Git::Push> plugin without
 the need to do a C<git push> between the C<dzil new> and C<dzil release>. Note
 though that this will work only when the C<push.default> Git configuration
 option is set to either C<upstream> or C<simple> (which will be the default in
-Git 2.0).
+Git 2.0). If you are using an older Git or don't want to change your config,
+you may want to have a look at L<Dist::Zilla::Plugin::Git::PushInitial>.
 
 =head1 AUTHOR
 
