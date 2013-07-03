b00e8f876f1773701268b5af2838cfa106f0cc8b
Karen Etheridge <ether@cpan.org>
2013-07-03 11:32:37 -0700

    make these messages show up all the time

--- lib/Dist/Zilla/Plugin/GitHub/Create.pm
+++ lib/Dist/Zilla/Plugin/GitHub/Create.pm
@@ -121,19 +121,19 @@ sub after_mint {
 	$params -> {'description'} = $opts -> {'descr'} if $opts -> {'descr'};
 
 	$params -> {'has_issues'} = $self -> has_issues;
-	$self -> log_debug($params -> {'has_issues'}   ?
+	$self -> log([ $params -> {'has_issues'}   ?
 				"Issues enabled" :
-				"Issues disabled");
+				"Issues disabled" ]);
 
 	$params -> {'has_wiki'} = $self -> has_wiki;
-	$self -> log_debug($params -> {'has_wiki'}   ?
+	$self -> log([ $params -> {'has_wiki'}   ?
 				"Wiki enabled" :
-				"Wiki disabled");
+				"Wiki disabled" ]);
 
 	$params -> {'has_downloads'} = $self -> has_downloads;
-	$self -> log_debug($params -> {'has_downloads'}   ?
+	$self -> log([ $params -> {'has_downloads'}   ?
 				"Downloads enabled" :
-				"Downloads disabled");
+				"Downloads disabled" ]);
 
 	my $url = $self -> api.'/user/repos';
 
