ghedo/p5-Dist-Zilla-Plugin-GitHub

commit 96d3b9c10e102508a0265d4fa26a2b71d35f3b93

Refactor handling of auth headers
This needs to be centralized so we can easily add these for the Meta request
as well.

lib/Dist/Zilla/Plugin/GitHub.pm
@@ -35,6 +35,13 @@ has prompt_2fa => (
     default => 0
 );
 
+has _credentials => (
+    is => 'ro',
+    isa => 'HashRef',
+    lazy => 1,
+    builder => '_build_credentials',
+);
+
 =head1 DESCRIPTION
 
 B<Dist::Zilla::Plugin::GitHub> is a set of plugins for L<Dist::Zilla> intended
@@ -58,7 +65,7 @@ bundle|Dist::Zilla::PluginBundle::GitHub>.
 
 =cut
 
-sub _get_credentials {
+sub _build_credentials {
     my $self = shift;
 
     my ($login, $pass, $token, $otp);
@@ -78,7 +85,7 @@ sub _get_credentials {
             "Err: Missing value 'github.user' in git config";
 
         $self->log($error);
-        return;
+        return [];
     }
 
     if (%identity) {
@@ -109,7 +116,32 @@ sub _get_credentials {
         );
     }
 
-    return ($login, $pass, $otp);
+    return {login => $login, pass => $pass, otp => $otp};
+}
+
+sub _has_credentials {
+    my $self = shift;
+    return keys %{$self->_credentials};
+}
+
+sub _auth_headers {
+    my $self = shift;
+
+    my $credentials = $self->_credentials;
+
+    my %headers;
+    if ($credentials->{pass}) {
+        require MIME::Base64;
+        my $basic = MIME::Base64::encode_base64("$credentials->{login}:$credentials->{pass}", '');
+        $headers{Authorization} = "Basic $basic";
+    }
+
+    if ($self->prompt_2fa) {
+        $headers{'X-GitHub-OTP'} = $credentials->{otp};
+        $self->log([ "Using two-factor authentication" ]);
+    }
+
+    return \%headers;
 }
 
 sub _get_repo_name {

lib/Dist/Zilla/Plugin/GitHub/Create.pm
@@ -111,8 +111,6 @@ sub after_mint {
         $repo_name = $self->zilla->name;
     }
 
-    my ($login, $pass, $otp)  = $self->_get_credentials;
-
     my $http = HTTP::Tiny->new;
 
     $self->log([ "Creating new GitHub repository '%s'", $repo_name ]);
@@ -139,23 +137,11 @@ sub after_mint {
     $url .= $self->org ? '/orgs/' . $self->org . '/' : '/user/';
     $url .= 'repos';
 
-    if ($pass) {
-        require MIME::Base64;
-
-        my $basic = MIME::Base64::encode_base64("$login:$pass", '');
-        $headers->{authorization} = "Basic $basic";
-    }
-
-    if ($self->prompt_2fa) {
-        $headers->{'X-GitHub-OTP'} = $otp;
-        $self->log([ "Using two-factor authentication" ]);
-    }
-
     $content = encode_json($params);
 
     my $response = $http->request('POST', $url, {
         content => $content,
-        headers => $headers
+        headers => $self->_auth_headers,
     });
 
     my $repo = $self->_check_response($response);

lib/Dist/Zilla/Plugin/GitHub/Update.pm
@@ -88,10 +88,9 @@ sub after_release {
     my ($opts)    = @_;
     my $dist_name = $self->zilla->name;
 
-    my ($login, $pass, $otp)  = $self->_get_credentials;
-    return if (!$login);
+    return if (!$self->_has_credentials);
 
-    my $repo_name = $self->_get_repo_name($login);
+    my $repo_name = $self->_get_repo_name($self->_credentials->{login});
     if (not $repo_name) {
         $self->log('cannot update GitHub repository info');
         return;
@@ -132,22 +131,9 @@ sub after_release {
         return;
     }
 
-    my $headers;
-
-    if ($pass) {
-        require MIME::Base64;
-        my $basic = MIME::Base64::encode_base64("$login:$pass", '');
-        $headers->{Authorization} = "Basic $basic";
-    }
-
-    if ($self->prompt_2fa) {
-        $headers->{'X-GitHub-OTP'} = $otp;
-        $self->log([ "Using two-factor authentication" ]);
-    }
-
     my $response = HTTP::Tiny->new->request('PATCH', $url, {
         content => encode_json($params),
-        headers => $headers
+        headers => $self->_auth_headers,
     });
 
     my $repo = $self->_check_response($response);

t/01-update.t
@@ -14,7 +14,7 @@ use Test::Deep::JSON;
     use Dist::Zilla::Plugin::GitHub;
     package Dist::Zilla::Plugin::GitHub;
     no warnings 'redefine';
-    sub _get_credentials { 'bob' }
+    sub _build_credentials { return {login => 'bob', pass => q{}, otp => q{}} }
     sub _get_repo_name { 'bob/My-Stuff' }
 }
 
@@ -44,7 +44,7 @@ my @tests = (
         ],
         expected_request => [
             PATCH => 'https://api.github.com/repos/bob/My-Stuff' => {
-                headers => undef,
+                headers => {},
                 content => json({
                     name => 'My-Stuff',
                     description => 'Sample DZ Dist',

commit 5ee2cba98b95b9cc09ed3acb54d7dbe3b56ef71a

Fix error caused by change 96d3b9c

lib/Dist/Zilla/Plugin/GitHub.pm
@@ -35,6 +35,13 @@ has prompt_2fa => (
     default => 0
 );
 
+has _login => (
+    is      => 'ro',
+    isa     => 'Maybe[Str]',
+    lazy    => 1,
+    builder => '_build_login',
+);
+
 has _credentials => (
     is => 'ro',
     isa => 'HashRef',
@@ -65,10 +72,10 @@ bundle|Dist::Zilla::PluginBundle::GitHub>.
 
 =cut
 
-sub _build_credentials {
+sub _build_login {
     my $self = shift;
 
-    my ($login, $pass, $token, $otp);
+    my ($login);
 
     my %identity = Config::Identity::GitHub->load
         if try_load_class('Config::Identity::GitHub');
@@ -85,8 +92,25 @@ sub _build_credentials {
             "Err: Missing value 'github.user' in git config";
 
         $self->log($error);
-        return [];
+        return undef;
     }
+
+    return $login;
+}
+
+sub _build_credentials {
+    my $self = shift;
+
+    my ($login, $pass, $token, $otp);
+
+    $login = $self->_login;
+
+    if (!$login) {
+        return {};
+    }
+
+    my %identity = Config::Identity::GitHub->load
+        if try_load_class('Config::Identity::GitHub');
 
     if (%identity) {
         $token = $identity{token};
@@ -165,7 +189,7 @@ sub _get_repo_name {
     $repo = $self->zilla->name unless $repo;
 
     if ($repo !~ /.*\/.*/) {
-        ($login, undef, undef) = $self->_get_credentials(1);
+        $login = $self->_login;
         if (defined $login) {
             $repo = "$login/$repo";
         }